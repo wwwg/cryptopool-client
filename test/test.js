c4dbaac67c5f7f7a35b76fcf5fa7723b6296f746
ultratype <ultratypebot@gmail.com>
2017-12-27 00:18:14 -0600

    add login to test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,8 @@
 const PoolClient = require('..');
+let c = new PoolClient('mine.durinsmine.com', 5555);
+console.log('connecting pool client...');
+c.on('connect', () => {
+    console.log('pool client connected');
+    c.login('Wms9U6et45NbXWfJmPVRVK3xQKnXnGcKJZRgv6A5cUPji6kLhL17aEW8ZMUbhzzYq8K2paEDg6WyDEgsm8yQg3nn2P8PCkbCP',
+        'x');
+});
