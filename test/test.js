0df21162b63ec21de58b5315cd3bd5507ba7dfc5
ultratype <ultratypebot@gmail.com>
2017-12-27 00:19:14 -0600

    add job events to test

--- test/test.js
+++ test/test.js
@@ -5,4 +5,6 @@ c.on('connect', () => {
     console.log('pool client connected');
     c.login('Wms9U6et45NbXWfJmPVRVK3xQKnXnGcKJZRgv6A5cUPji6kLhL17aEW8ZMUbhzzYq8K2paEDg6WyDEgsm8yQg3nn2P8PCkbCP',
         'x');
+}).on('job', j => {
+    console.log('recieved job', j);
 });
